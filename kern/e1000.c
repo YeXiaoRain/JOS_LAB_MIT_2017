2dfa679aa6cbed846375434d8d7c7088aeb12740
yexiaorain <yexiaorain@gmail.com>
2017-06-11 16:10:26 -0700

    lab6 PartA(4/5)

--- kern/e1000.c
+++ kern/e1000.c
@@ -1,10 +1,36 @@
 #include <kern/e1000.h>
 
+struct e1000_tx_desc tx_queue[E1000_NTX] __attribute__((aligned(16)));
+char tx_pkt_bufs[E1000_NTX][TX_PKT_SIZE];
+
 int e1000_attach(struct pci_func *f) {
   pci_func_enable(f);
 
   boot_map_region(kern_pgdir, E1000_BASE, f->reg_size[0], f->reg_base[0], PTE_PCD | PTE_PWT | PTE_W);
   e1000 = (uint32_t *)E1000_BASE;
   assert(e1000[E1000_STATUS >> 2] == 0x80080783);
+
+  memset(tx_queue   , 0, sizeof(tx_queue));
+  memset(tx_pkt_bufs, 0, sizeof(tx_pkt_bufs));
+  int i;
+  for (i = 0; i < E1000_NTX; i++) {
+    tx_queue[i].buffer_addr = PADDR(tx_pkt_bufs[i]);
+    tx_queue[i].upper.data |= E1000_TXD_STAT_DD;
+  }
+
+  e1000[E1000_TDBAL >> 2] = PADDR(tx_queue);
+  e1000[E1000_TDBAH >> 2] = 0;
+  e1000[E1000_TDLEN >> 2] = sizeof(tx_queue);
+  e1000[E1000_TDH   >> 2] = 0;
+  e1000[E1000_TDT   >> 2] = 0;
+
+  e1000[E1000_TCTL  >> 2] |= E1000_TCTL_EN;
+  e1000[E1000_TCTL  >> 2] |= E1000_TCTL_PSP;
+  e1000[E1000_TCTL  >> 2] &= ~E1000_TCTL_CT;  //0x000ff0
+  e1000[E1000_TCTL  >> 2] |= (0x10) << 4;     //0x000100
+  e1000[E1000_TCTL  >> 2] &= ~E1000_TCTL_COLD;//0x3ff000
+  e1000[E1000_TCTL  >> 2] |= (0x40) << 12;    //0x012000
+  e1000[E1000_TIPG  >> 2]  = 10 | (4 << 10) | (6 << 20); // IPGT | IPGR1 | IPGR2
+
   return 0;
 }
--- kern/e1000.h
+++ kern/e1000.h
@@ -6,12 +6,51 @@
 
 #define E1000_BASE KSTACKTOP
 
+#define E1000_NTX       64
+#define TX_PKT_SIZE     1518
+
 #define E1000_STATUS    0x00008  /* Device Status - RO */
+#define E1000_TDBAL     0x03800  /* TX Descriptor Base Address Low - RW */
+#define E1000_TDBAH     0x03804  /* TX Descriptor Base Address High - RW */
+#define E1000_TDLEN     0x03808  /* TX Descriptor Length - RW */
+#define E1000_TDH       0x03810  /* TX Descriptor Head - RW */
+#define E1000_TDT       0x03818  /* TX Descripotr Tail - RW */
+#define E1000_TCTL      0x00400  /* TX Control - RW */
+#define E1000_TIPG      0x00410  /* TX Inter-packet gap -RW */
+
+#define E1000_TCTL_EN   0x00000002    /* enable tx */
+#define E1000_TCTL_PSP  0x00000008    /* pad short packets */
+#define E1000_TCTL_CT   0x00000ff0    /* collision threshold */
+#define E1000_TCTL_COLD 0x003ff000    /* collision distance */
+
+#define E1000_TXD_STAT_DD    0x00000001 /* Descriptor Done */
+
+#include <inc/string.h>
 #include <kern/pci.h>
 #include <kern/pmap.h>
 
 uint32_t *volatile e1000;
 
 int e1000_attach(struct pci_func *f);
+/* Transmit Descriptor */
+struct e1000_tx_desc {
+  uint64_t buffer_addr;       /* Address of the descriptor's data buffer */
+  union {
+    uint32_t data;
+    struct {
+      uint16_t length;    /* Data buffer length */
+      uint8_t cso;        /* Checksum offset */
+      uint8_t cmd;        /* Descriptor control */
+    } flags;
+  } lower;
+  union {
+    uint32_t data;
+    struct {
+      uint8_t status;     /* Descriptor status */
+      uint8_t css;        /* Checksum start */
+      uint16_t special;
+    } fields;
+  } upper;
+};
 
 #endif	// JOS_KERN_E1000_H
